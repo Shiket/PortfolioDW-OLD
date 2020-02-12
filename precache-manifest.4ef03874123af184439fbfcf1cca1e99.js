52e0cabfd1e4016248602b6cd207fda956687ffd
Shiket <shiket95@gmail.com>
2020-02-12 12:40:01 +0100

    Updates

---
+++ precache-manifest.4ef03874123af184439fbfcf1cca1e99.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7f0b7a34f452293eec740135308a459",
+    "url": "/PortfolioDW/index.html"
+  },
+  {
+    "revision": "db75c1981ae0a8edd802",
+    "url": "/PortfolioDW/static/js/2.595b083b.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/PortfolioDW/static/js/2.595b083b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc0cbf55283fc7410b65",
+    "url": "/PortfolioDW/static/js/main.33d72f25.chunk.js"
+  },
+  {
+    "revision": "54a3c7f6868f8541173d",
+    "url": "/PortfolioDW/static/js/runtime-main.cf97417e.js"
+  },
+  {
+    "revision": "d223ac8e7622635d1b03e425d540b5cf",
+    "url": "/PortfolioDW/static/media/avatar.d223ac8e.png"
+  },
+  {
+    "revision": "458a5518235698d85a0448a05649e061",
+    "url": "/PortfolioDW/static/media/bootsrapIcon.458a5518.png"
+  },
+  {
+    "revision": "525db5cda4e961bea00a06eef8393012",
+    "url": "/PortfolioDW/static/media/cssicon.525db5cd.png"
+  },
+  {
+    "revision": "dcadc66dce6d7f175d36bc14af5087df",
+    "url": "/PortfolioDW/static/media/html5Icon.dcadc66d.png"
+  },
+  {
+    "revision": "c1bfac1152bf0495e99e81569425ca67",
+    "url": "/PortfolioDW/static/media/linkedIcon.c1bfac11.png"
+  },
+  {
+    "revision": "957f66f35cee72fb8b04e968c1ec5220",
+    "url": "/PortfolioDW/static/media/photoshopIcon.957f66f3.png"
+  },
+  {
+    "revision": "bc788aad09159bfee1ca81a580338afc",
+    "url": "/PortfolioDW/static/media/project1.bc788aad.png"
+  },
+  {
+    "revision": "bc0715099e948226acff59f7ff989082",
+    "url": "/PortfolioDW/static/media/project2.bc071509.png"
+  },
+  {
+    "revision": "7f0f8d15ff1e413be17134c6250f7113",
+    "url": "/PortfolioDW/static/media/reactIcon48.7f0f8d15.png"
+  },
+  {
+    "revision": "1e5a4ccd36227bc56fa3269dda868795",
+    "url": "/PortfolioDW/static/media/styledIcon.1e5a4ccd.png"
+  },
+  {
+    "revision": "c6e14b4a9d28baf2396c86c78407c1d2",
+    "url": "/PortfolioDW/static/media/whiteGithub.c6e14b4a.png"
+  },
+  {
+    "revision": "f02075ab7ad7a00091cd66862b0cd9da",
+    "url": "/PortfolioDW/static/media/xdIcon.f02075ab.png"
+  }
+]);
